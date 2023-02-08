b62d2a2db4958fdc29128541b058a9ea9164e805
Dominik Magic <dominikmagic5@gmail.com>
2023-02-08 13:12:11 -0800

    app will display the list of merchandise
    o-Authored-By: Robert Onstott
    <roonstott@gmail.com>

---
+++ src/components/TicketList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Ticket from './Ticket';
+import PropTypes from "prop-types";
+
+
+
+
+function TicketList(props){
+  return (
+    <React.Fragment>
+      <hr/>
+        {props.ticketList.map((ticket, index) =>
+          <Ticket names={ticket.names}
+            location={ticket.location}
+            issue={ticket.issue}
+            key={index}/>
+        )}
+    </React.Fragment>
+  );
+}
+
+//Add propTypes for ticketList.
+TicketList.propTypes = {
+  ticketList: PropTypes.array
+};
+
+export default TicketList;
